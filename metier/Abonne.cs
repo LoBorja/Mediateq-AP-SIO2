0765e3df3d30a1f56220f8d6ae7940fc3f467c20
loborja <loris.borja@clsi.fr>
2023-05-29 23:37:11 +0200

    Commentaire XML

--- metier/Abonne.cs
+++ metier/Abonne.cs
@@ -21,6 +21,18 @@ namespace Mediateq_AP_SIO2.metier
         private DateTime finAbonnement;
         private string mailU;
 
+        /// <summary>
+        /// Constructeur Abonne
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="nom"></param>
+        /// <param name="prenom"></param>
+        /// <param name="dateNaissance"></param>
+        /// <param name="adresse"></param>
+        /// <param name="numTel"></param>
+        /// <param name="typeAbonnement"></param>
+        /// <param name="finAbonnement"></param>
+        /// <param name="mailU"></param>
         public Abonne(string id, string nom, string prenom, DateTime dateNaissance, string adresse, string numTel, string typeAbonnement, DateTime finAbonnement, string mailU)
         {
             this.id = id;
@@ -34,14 +46,49 @@ namespace Mediateq_AP_SIO2.metier
             this.mailU = mailU;
         }
 
+        /// <summary>
+        /// getter setter Id
+        /// </summary>
         public string Id { get => id; set => id = value; }
+
+        /// <summary>
+        /// getter setter Nom
+        /// </summary>
         public string Nom { get => nom; set => nom = value; }
+
+        /// <summary>
+        /// getter setter Prenom
+        /// </summary>
         public string Prenom { get => prenom; set => prenom = value; }
+
+        /// <summary>
+        /// getter setter Date de Naissance
+        /// </summary>
         public DateTime DateNaissance { get => dateNaissance; set => dateNaissance = value; }
+
+        /// <summary>
+        /// getter setter Adresse
+        /// </summary>
         public string Adresse { get => adresse; set => adresse = value; }
+
+        /// <summary>
+        /// getter setter Numéro de téléphone
+        /// </summary>
         public string NumTel { get => numTel; set => numTel = value; }
+
+        /// <summary>
+        /// getter setter Type d'abonnement
+        /// </summary>
         public string TypeAbonnement { get => typeAbonnement; set => typeAbonnement = value; }
+
+        /// <summary>
+        /// getter setter Date de fin d'abonnement
+        /// </summary>
         public DateTime FinAbonnement { get => finAbonnement; set => finAbonnement = value; }
+
+        /// <summary>
+        /// getter setter Mail de l'abonné
+        /// </summary>
         public string MailU { get => mailU; set => mailU = value; }
     }
 }
